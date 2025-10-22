ad80755801d5d3ab72c7f2e2c8f3aa44a4f66606
Alexandra138-sketch <estgv19355@alunos.estgv.ipv.pt>
2025-10-22 10:58:31 +0100

    Classes

--- gestaodefinanca.cs
+++ gestaodefinanca.cs
@@ -1,3 +1,30 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+namespace gestaodefinanca
+{
+    class Utilizador
+    {
+        private string nome;
+        private string email;
+        private string password;
+    }
+    class Despesa
+    {
+        private int id;
+        public decimal Valor;
+        public DateTime Data;
+        public string Descricao;
+        public Categoria Categoria;
+        public Utilizador Utilizador;
+    }
+    class Receita
+    {
+        private int id;
+        public decimal Valor;
+        public DateTime Data;
+        public string Descricao;
+        public Categoria Categoria;
+        public Utilizador Utilizador;
+    }
+}
