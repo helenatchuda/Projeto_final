3ab06c9af94953ba3689a9a9ee5a48ca0d9912f3
helenatchuda <helenatchuda@gmail.com>
2025-11-05 12:37:31 +0000

    implementacao das class

---
+++ ReceitaController.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class ReceitaController
+{
+    public void inicialFormReceita()
+    {
+        
+    }
+
+    public void criaReceita(double valor, DateTime data, Categoria categoria, string descricao, int utilizador_id)
+    {
+        
+    }
+}
